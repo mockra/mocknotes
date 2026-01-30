ea0b50f7d929da4756f840d5faae9a5c9914d5d2
David Ratajczak <david@mockra.com>
2026-01-29 21:28:38 -0800

    Add error msg to save

--- lua/mocknotes/save.lua
+++ lua/mocknotes/save.lua
@@ -10,6 +10,10 @@ function M.save_all()
 		vim.fn.shellescape(save_message)
 	)
 	local result = vim.fn.system(cmd)
+
+	if vim.v.shell_error ~= 0 then
+		vim.notify("MockNotes: Failed to auto save notes:\n" .. result, vim.log.levels.ERROR)
+	end
 end
 
 return M
